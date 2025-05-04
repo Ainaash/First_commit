996a9b38d508007b3ba133dde7a48813986fac7f
Ainash <aakmatova179@gmail.com>
2025-05-05 02:22:37 +0600

    first

---
+++ Work_set2.py
@@ -0,0 +1,6 @@
+day=['Дуйшомбу','Шейшемби','Шаршемби']
+fruit=['алма','банан', 'жузум']
+drink=['чай','сок','кофе']
+food=['ботко','шорпо','аш']
+for day,fruit,drink,food in zip (day,fruit,drink,food):
+    print (day,":ичууго",drink,"пайдалуу жемиш ",fruit," жана тамактанууга",food)
