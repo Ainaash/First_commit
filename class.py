4a73c6a45fa912a4edc02458c8aad5dc42677530
Ainash <aakmatova179@gmail.com>
2025-05-12 07:34:45 +0600

    first

---
+++ class.py
@@ -0,0 +1,20 @@
+class Person():
+    def __init__(self, name):
+        self.name = name
+
+class MDPerson(Person):
+    def __init__(self, name):
+        self.name = "Teacher " + name
+
+class JDPerson(Person):
+    def __init__(self, name):
+        self.name = name + ", Umar"
+
+person = Person('Fudd')
+doctor = MDPerson('Fudd')
+lawyer = JDPerson('Fudd')
+print(person.name)
+
+print(doctor.name)
+
+print(lawyer.name)
